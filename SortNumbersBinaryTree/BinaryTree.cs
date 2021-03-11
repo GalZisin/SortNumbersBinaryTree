a8a075e1dd019dc2f501fb4c74a1143bed135e26
Gal Zisin <galzisin86@gmail.com>
2021-03-11 23:25:49 +0200

    Add project files.

---
+++ SortNumbersBinaryTree/BinaryTree.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortNumbersBinaryTree
+{
+    public class BinaryTree
+    {
+        private int arrayLength;
+        private string filePathOutput;
+
+        // Class containing left and
+        // right child of current 
+        // node and key value
+        public class Node
+        {
+            public int key;
+            public Node left, right;
+
+            public Node(int item)
+            {
+                key = item;
+                left = right = null;
+            }
+        }
+
+        // Root of BST
+        public Node root;
+
+        // Constructor
+        public BinaryTree(int arrayLength, string filePathOutput)
+        {
+            root = null;
+            this.arrayLength = arrayLength;
+            this.filePathOutput = filePathOutput;
+        }
+
+        // This method mainly
+        // calls insertRec()
+        public void insert(int key)
+        {
+            root = insertRec(root, key);
+        }
+
+        /* A recursive function to 
+          insert a new key in BST */
+        Node insertRec(Node root, int key)
+        {
+
+            /* If the tree is empty,
+                return a new node */
+            if (root == null)
+            {
+                root = new Node(key);
+                return root;
+            }
+
+            /* Otherwise, recur
+                down the tree */
+            if (key < root.key)
+                root.left = insertRec(root.left, key);
+            else if (key > root.key)
+                root.right = insertRec(root.right, key);
+
+            /* return the root */
+            return root;
+        }
+
+        // A function to do 
+        // inorder traversal of BST
+        public void inorderRec(Node root)
+        {
+ 
+            if (root != null)
+            {
+                inorderRec(root.left);
+                Console.Write(root.key + " ");
+                AppendToFile(root.key.ToString());
+                inorderRec(root.right);
+            }
+           
+        }
+        public void treeins(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                insert(arr[i]);
+            }
+
+        }
+
+        public void AppendToFile(string number)
+        {
+            using (StreamWriter w = File.AppendText(filePathOutput))
+            {
+                w.WriteLine(number);
+            }
+        }
+    }
+}
---
+++ SortNumbersBinaryTree/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortNumbersBinaryTree
+{
+    class Program
+    {
+        private static string filePathInput = "RandomNumbers.txt";
+        private static string filePathOutput = "SortedNumbers.txt";
+        public static void Main(string[] args)
+        {
+            string[] rendomNumbers = File.ReadAllLines(filePathInput);
+            int[] rendomNumbersArray = new int[rendomNumbers.Length];
+            int i = 0;
+            foreach (String number in rendomNumbers)
+            {
+                try
+                {
+                    int result = Int32.Parse(number);
+
+                    rendomNumbersArray[i] = result;
+                    i++;
+                    //Console.WriteLine(result);
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine($"Unable to parse '{number}'");
+                }
+            }
+
+            BinaryTree tree = new BinaryTree(rendomNumbersArray.Length, filePathOutput);
+
+            tree.treeins(rendomNumbersArray);
+            tree.inorderRec(tree.root);
+
+            Console.ReadLine();
+        }
+    
+    }
+
+}
+
